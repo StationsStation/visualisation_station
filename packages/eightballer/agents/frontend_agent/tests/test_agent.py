755511dc075d6c41d81c6d38008634372636b37e
xiuxiuxar <174127740+xiuxiuxar@users.noreply.github.com>
2024-12-21 10:10:48 -0800

    Fix custom import, add agent test

--- packages/eightballer/agents/frontend_agent/tests/test_agent.py
+++ packages/eightballer/agents/frontend_agent/tests/test_agent.py
@@ -1,54 +1,77 @@
-"""
-Base Custom Tests for test.
-"""
+"""Base Custom Tests for test."""
 
+import json
 import subprocess
 from pathlib import Path
 
+import requests
+from aea.configurations.constants import (
+    DEFAULT_PRIVATE_KEY_FILE,
+    LAUNCH_SUCCEED_MESSAGE,
+)
 from aea.test_tools.test_cases import AEATestCaseMany
-from aea_test_autonomy.configurations import ANY_ADDRESS, DEFAULT_REQUESTS_TIMEOUT
+from aea_test_autonomy.configurations import DEFAULT_REQUESTS_TIMEOUT
+from web3 import Web3
 
+DEFAULT_ENCODING = "utf-8"
+TINDERMINT_URL = "http://localhost:8080/hard_reset"
+TERMINATION_TIMEOUT = 30
+DEFAULT_LAUNCH_TIMEOUT = 30
 AGENT_NAME = "frontend_agent"
 AUTHOR = "eightballer"
 VERSION = "0.1.0"
-DEFAULT_LAUNCH_TIMEOUT = 30
-LAUNCH_SUCCEED_MESSAGE = "Start processing"
+HANDLER_SUCCEED_MESSAGE = "Handler PingPongHandler loaded."
 
 
-class TestAgentLaunch(
-    AEATestCaseMany,
-):
+class TestAgentLaunch(AEATestCaseMany):
     """Test that the Agent launches."""
 
     IS_LOCAL = True
     capture_log = True
-    cli_log_options = ["-v", "DEBUG"]
+    cli_log_options = ["-v", "INFO"]
     package_registry_src_rel = Path(__file__).parent.parent.parent.parent.parent
 
     def test_run(self) -> None:
         """Run the ABCI skill."""
-        agent_name = "base"
+        test_agent_name = "base"
         self.fetch_agent(
-            f"{AUTHOR}/{AGENT_NAME}:{VERSION}", agent_name, is_local=self.IS_LOCAL
+            f"{AUTHOR}/{AGENT_NAME}:{VERSION}", test_agent_name, is_local=self.IS_LOCAL
         )
-        self.set_agent_context(agent_name)
+        self.set_agent_context(test_agent_name)
         self.generate_private_key("ethereum")
-        self.generate_private_key("cosmos")
-        self.add_private_key("ethereum", "ethereum_private_key.txt")
-        self.add_private_key("cosmos", "cosmos_private_key.txt")
-        self.invoke(
-            "issue-certificates",
-        )
+
+        with open(
+            f"{test_agent_name}/{DEFAULT_PRIVATE_KEY_FILE}", encoding=DEFAULT_ENCODING
+        ) as f:
+            self.eth_address = Web3().eth.account.from_key(f.read()).address
+
+        self.add_private_key("ethereum", DEFAULT_PRIVATE_KEY_FILE)
+        self._set_configs()
+
+        requests.get(TINDERMINT_URL, timeout=DEFAULT_REQUESTS_TIMEOUT)
+
+        self.invoke("issue-certificates")
         process = self.run_agent()
         is_running = self.is_running(process)
         assert is_running, "AEA not running within timeout!"
+        assert not self.missing_from_output(
+            process, (HANDLER_SUCCEED_MESSAGE,), timeout=30, is_terminating=False
+        ), "Custom handler is not loaded!"
+
+        # Kill process
+        process.kill()
+        process.wait()
+
+        # Terminate agents
+        assert (
+            self.terminate_agents(timeout=TERMINATION_TIMEOUT) is None
+        ), "Failed to terminate agents within timeout"
 
     @classmethod
     def is_running(
         cls, process: subprocess.Popen, timeout: int = DEFAULT_LAUNCH_TIMEOUT
     ) -> bool:
-        """
-        Check if the AEA is launched and running (ready to process messages).
+        """Check if the AEA is launched and running (ready to process messages).
 
         :param process: agent subprocess.
         :param timeout: the timeout to wait for launch to complete
@@ -59,3 +82,12 @@ class TestAgentLaunch(
         )
 
         return missing_strings == []
+
+    def _set_configs(self) -> None:
+        """Set the current agent's config overrides."""
+
+        self.set_config(
+            f"vendor.{AUTHOR}.skills.trader_abci.models.params.args.setup.all_participants",
+            json.dumps([self.eth_address]),
+            "list",
+        )
