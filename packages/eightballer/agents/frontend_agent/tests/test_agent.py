34985926972e8fb54be60d8634c75fdf65a94d49
xiuxiuxar <174127740+xiuxiuxar@users.noreply.github.com>
2025-01-01 10:37:10 -0800

    WIP: usetendermint fixture

--- packages/eightballer/agents/frontend_agent/tests/test_agent.py
+++ packages/eightballer/agents/frontend_agent/tests/test_agent.py
@@ -4,17 +4,27 @@ import json
 import subprocess
 from pathlib import Path
 
+import pytest
 import requests
-from aea.configurations.constants import (
-    DEFAULT_PRIVATE_KEY_FILE,
-    LAUNCH_SUCCEED_MESSAGE,
-)
-from aea.test_tools.test_cases import AEATestCaseMany
-from aea_test_autonomy.configurations import DEFAULT_REQUESTS_TIMEOUT
 from web3 import Web3
+from aea.test_tools.test_cases import AEATestCaseMany
+from aea.configurations.constants import (
+    LAUNCH_SUCCEED_MESSAGE,
+    DEFAULT_PRIVATE_KEY_FILE,
+)
+from aea_test_autonomy.configurations import ANY_ADDRESS, DEFAULT_REQUESTS_TIMEOUT
+from aea_test_autonomy.fixture_helpers import (
+    UseTendermint,
+    abci_host,
+    abci_port,
+    tendermint,
+    ipfs_daemon,
+    tendermint_port,
+)
+
 
 DEFAULT_ENCODING = "utf-8"
-TINDERMINT_URL = "http://localhost:8080/hard_reset"
+TENDERMINT_URL = "http://localhost:8080/hard_reset"
 TERMINATION_TIMEOUT = 30
 DEFAULT_LAUNCH_TIMEOUT = 30
 AGENT_NAME = "frontend_agent"
@@ -23,32 +33,31 @@ VERSION = "0.1.0"
 HANDLER_SUCCEED_MESSAGE = "Handler PingPongHandler loaded."
 
 
-class TestAgentLaunch(AEATestCaseMany):
+@pytest.mark.usefixtures("tendermint", "tendermint_port", "abci_host", "abci_port")
+class TestAgentLaunch(AEATestCaseMany, UseTendermint):
     """Test that the Agent launches."""
 
     IS_LOCAL = True
     capture_log = True
-    cli_log_options = ["-v", "INFO"]
+    cli_log_options = ["-v", "DEBUG"]
     package_registry_src_rel = Path(__file__).parent.parent.parent.parent.parent
 
     def test_run(self) -> None:
         """Run the ABCI skill."""
-        test_agent_name = "base"
+        agent_name = "base"
         self.fetch_agent(
-            f"{AUTHOR}/{AGENT_NAME}:{VERSION}", test_agent_name, is_local=self.IS_LOCAL
+            f"{AUTHOR}/{AGENT_NAME}:{VERSION}", agent_name, is_local=self.IS_LOCAL
         )
-        self.set_agent_context(test_agent_name)
+        self.set_agent_context(agent_name)
         self.generate_private_key("ethereum")
 
         with open(
-            f"{test_agent_name}/{DEFAULT_PRIVATE_KEY_FILE}", encoding=DEFAULT_ENCODING
+            f"{agent_name}/{DEFAULT_PRIVATE_KEY_FILE}", encoding=DEFAULT_ENCODING
         ) as f:
             self.eth_address = Web3().eth.account.from_key(f.read()).address
 
         self.add_private_key("ethereum", DEFAULT_PRIVATE_KEY_FILE)
         self._set_configs()
-
-        requests.get(TINDERMINT_URL, timeout=DEFAULT_REQUESTS_TIMEOUT)
 
         self.invoke("issue-certificates")
         process = self.run_agent()
@@ -90,4 +99,12 @@ class TestAgentLaunch(AEATestCaseMany):
             f"vendor.{AUTHOR}.skills.trader_abci.models.params.args.setup.all_participants",
             json.dumps([self.eth_address]),
             "list",
+        )
+        self.set_config(
+            "vendor.valory.connections.abci.config.host",
+            ANY_ADDRESS,
+        )
+        self.set_config(
+            "vendor.valory.connections.abci.config.port",
+            self.abci_port,
         )
